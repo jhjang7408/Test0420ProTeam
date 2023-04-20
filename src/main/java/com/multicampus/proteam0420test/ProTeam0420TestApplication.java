be825afae36a708c26d5e9504e6abca07b184cd6
Junhwan <jhjang7408@naver.com>
2023-04-20 14:02:05 +0900

    test04200201

---
+++ src/main/java/com/multicampus/proteam0420test/ProTeam0420TestApplication.java
@@ -0,0 +1,13 @@
+package com.multicampus.proteam0420test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTeam0420TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProTeam0420TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multicampus/proteam0420test/ProTeam0420TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicampus.proteam0420test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTeam0420TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
