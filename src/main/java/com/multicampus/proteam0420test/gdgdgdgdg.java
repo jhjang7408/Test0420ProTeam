b1d937c229907c781a042ef9c92470451b2817ee
Junhwan <jhjang7408@naver.com>
2023-04-20 14:28:42 +0900

    test0222222222

---
+++ src/main/java/com/multicampus/proteam0420test/gdgdgdgdg.java
@@ -0,0 +1,21 @@
+package com.multicampus.proteam0420test;
+
+public class gdgdgdgdg {
+
+
+    asdg
+    asdg
+            asdg
+    asdgasd
+
+    public asdg getAsdg() {
+
+        asdg
+                a
+                asdgasdg
+                        as
+                        dg
+                                asdg
+                                asdg
+    }
+}
